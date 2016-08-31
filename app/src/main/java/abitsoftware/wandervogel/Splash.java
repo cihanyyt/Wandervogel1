454148f46c39e35f6dd368c6f23912cbbb8d4824
Yesilyurt, Cihan (CT DD DS EU TR DF FA-2) <cihan.yesilyurt@siemens.com>
2016-08-31 16:39:11 +0300

    Initial commit

---
+++ app/src/main/java/abitsoftware/wandervogel/Splash.java
@@ -0,0 +1,43 @@
+package abitsoftware.wandervogel;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //fullscreen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        setContentView(R.layout.splash);
+
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(5000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    Intent openStartingPoint = new Intent("abitsoftware.wandervogel.MAPSACTIVITY");
+                    startActivity(openStartingPoint);
+                }
+            }
+        };
+        timer.start();
+    }
+
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+}
